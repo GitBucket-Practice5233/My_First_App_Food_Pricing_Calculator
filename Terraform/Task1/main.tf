acb5f8937dd9bf3d8a030127d4baebe012d8e5ec
Swastik <swastiksenapati22@gmail.com>
2025-07-24 03:33:52 +0530

    up

--- Terraform/Task1/main.tf
+++ Terraform/Task1/main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "test" {
-  bucket = "First-Terraform-Bucket5233ygjjygjfhfcghfcjvgg"
+  bucket = "First-Terraform-Bucket5233ygjjygjfhfcghfcjvgg-hgjhv-jgyj"
 
   tags = {
     Name        = "My bucket"
