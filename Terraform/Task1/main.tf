66f75e62677af689dde0445f80725c2816b24515
Swastik <swastiksenapati22@gmail.com>
2025-07-23 16:32:18 +0530

    up

---
+++ Terraform/Task1/main.tf
@@ -0,0 +1,29 @@
+
+terraform 
+{
+  required_providers 
+  {
+    aws = 
+    {
+      source = "hashicorp/aws"
+      version = "5.91.0"
+    }
+  }
+}
+
+provider "aws" 
+{
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example" 
+{
+  bucket = "First-Terraform-Bucket"
+
+  tags = 
+  {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
